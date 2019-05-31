566930c1b5c7c64dbd4d09af7f39e65450de0fe2
sinid <49916315+oinksu@users.noreply.github.com>
2019-05-31 04:01:57 -0400

    added variables to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,40 @@
-// words list
-var words = ["RUGRATS", "SPONGEBOB", "POKEMON", "ANIMANIACS", "RECESS", "DOUG", "CATDOG", "SIMPSONS"];
 
-var maxNumGuesses = 8; // max number of guesses 
-var guessedLetters = []; // store the guessed letters
-var ansWordArr = []; // store the "_" and to be used to replace the word answer
-var numGuessesRemaining = 0; // number of guesses remaining
-var numWins = 0; // number of wins
-var numLosses = 0; // number of losses
-var isFinished = false; // when true, game can start again
-var ansWord; // the word that is being played
+var words = ["CHACALON", "GRUPOCELESTE", "JUANECO", "DESTELLOS", "DIABLOSROJOS", "HIJOSDELSOL", "LOSMIRLOS", "RIBERENOS", "SHAPIS", "WEMBLERSDEIQUITOS","PINTURAROJA"];
 
-// function runs at the start of page and used to restart after game isFinished
+var maxGuess = 8; 
+var Letters = []; 
+var ansWordArr = []; 
+var numWins = 0; 
+var numLosses = 0;
+var numGuessesRemaining = 0;
+var ansWord;
+var isFinished = false; 
+
+
 function setup() {
-    //picks random word from words list
+
     ansWord = words[Math.floor(Math.random() * words.length)];
 
     ansWordArr = [];
 
-    // adds "_" to ansWordArr
+ 
     for (var i = 0; i < ansWord.length; i++) {
         ansWordArr[i] = "_";
     }
 
-    // reset the variables 
-    numGuessesRemaining = maxNumGuesses;
+    numGuessesRemaining = maxGuess;
     guessedLetters = [];
 
-    //clears giphy-embed to now show any gifs
-    document.getElementById("giphy-embed").src = "";
-    //removes color from numGuesses
+    
+    document.getElementById("picResult").src = "assets/images/cumbia.png";
+   
     document.getElementById("numGuesses").style.color = "";
 
-    //show the selected elements on the screen 
+    
     updateScreen();
 };
 
-//updates the HTML from the functions
+
 function updateScreen() {
     document.getElementById("numWins").innerText = numWins;
     document.getElementById("numLosses").innerText = numLosses;
@@ -45,19 +44,19 @@ function updateScreen() {
 
 };
 
-//function to check the key that's pressed
-function checkGuess(letter) {
-    //if letter is not in guessedLetters array then push the letter to the array
+
+function check(letter) {
+   
     if (guessedLetters.indexOf(letter) === -1) {
         guessedLetters.push(letter);
-        //if the letter isn't in the answer word then -1 the numGuessesRemaining
+       
         if (ansWord.indexOf(letter) === -1) {
             numGuessesRemaining--;
-            //if numGuessesRemaining is 3 or less then change the color
+            
             if (numGuessesRemaining <=3) {
-                document.getElementById("numGuesses").style.color = "#e12d2e";
+                document.getElementById("numGuesses").style.color = "#fff";
             }
-            //if letter is in answer then replace the positioned "_" with the letter
+           
         } else { 
             for (var i = 0; i < ansWord.length; i++) {
                 if (letter === ansWord[i]) {
@@ -69,59 +68,82 @@ function checkGuess(letter) {
 
 }; 
 
-//function to check if the player is a winner
+
 function isWinner() {
-    //if there are no more "_" in the ansWordArr then +1 to Wins and switch isFinished to true
+    
     if (ansWordArr.indexOf("_") === -1) {
         numWins++;
         isFinished = true;
-        //if the answer is guessed then play assigned gif
-        if(ansWord === "DOUG") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/w7iOaLoi84N6E";
-        } else if (ansWord === "RUGRATS") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/3x5V8j8T341lS";
-        } else if (ansWord === "SPONGEBOB") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/TdfyKrN7HGTIY";
-        } else if (ansWord === "POKEMON") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/xuXzcHMkuwvf2";
-        } else if (ansWord === "ANIMANIACS") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/Vpu0dyuOVbrBC";
-        } else if (ansWord === "RECESS") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/ENjchsyk8aSoE";
-        } else if (ansWord === "CATDOG") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/VqWjJR7vOwmSk";
-        } else if (ansWord === "SIMPSONS") {
-            document.getElementById("giphy-embed").src = "https://giphy.com/embed/tkYpAbKdWj4TS";
+      
+        if(ansWord === "CHACALON") {
+            document.getElementById("picResult").src = "assets/images/Chacalon_Y_la_nueva_crema.jpg";
+        } 
+        
+        else if (ansWord === "GRUPOCELESTE") {
+            document.getElementById("picResult").src = "assets/images/grupo_celeste.jpg";
+        } 
+        
+        else if (ansWord === "JUANECO") {
+            document.getElementById("picResult").src = "assets/images/Juaneco_y_su_Combo.jpg";
+        } 
+        
+        else if (ansWord === "DESTELLOS") {
+            document.getElementById("picResult").src = "assets/images/Los_Destellos.jpg";
+        } 
+        
+        else if (ansWord === "DIABLOSROJOS") {
+            document.getElementById("picResult").src = "assets/images/Los_Diablos_Rojos.jpg";
+        } 
+        
+        else if (ansWord === "HIJOSDELSOL") {
+            document.getElementById("picResult").src = "assets/images/los_hijos_del_sol.jpg";
+        } 
+        
+        else if (ansWord === "LOSMIRLOS") {
+            document.getElementById("picResult").src = "assets/images/los_mirlos.jpg";
+        } 
+        
+        else if (ansWord === "RIBERENOS") {
+            document.getElementById("picResult").src = "assets/images/los_ribereNos.jpg";
+        } 
+        
+        else if (ansWord === "SHAPIS") {
+            document.getElementById("picResult").src = "assets/images/los_shapis.jpg";
+        } 
+        
+        else if (ansWord === "WEMBLERSDEIQUITOS") {
+            document.getElementById("picResult").src = "assets/images/los_wemblers_de_Iquitos.jpg";
+        } 
+        
+        else if (ansWord === "PINTURAROJA") {
+            document.getElementById("picResult").src = "assets/images/Pintura_Roja.jpg";
         }
             
     }
 };
-//function to check if player is a loser
+
 function isLoser() {
-    // if the numGuessesRemaining is 0 then -1 numLosses and switch isFinished to true
+    
     if(numGuessesRemaining <= 0) {
         numLosses++;
         isFinished = true;
-        //play the loser gif
-        document.getElementById("giphy-embed").src = "https://giphy.com/embed/3oFzmko6SiknmpR2NO";
+       
+        document.getElementById("picResult").src = "https://youtu.be/M5QGkOGZubQ";
         document.getElementById("numLosses").style.color = "#e12d2e";
     }
 
 };
 
 
-//event listener for key pressed
 document.onkeyup = function(event) {
-    //if isFinished is true then restart the game to the initial setup 
-    //and switch isFinished back to false
+   
     if (isFinished) {
         setup();
         isFinished = false;
     } else {
-        //check to see if only letters A-Z are pressed
-        //functions are executed when user presses A-Z key
+        
         if(event.keyCode >= 65 && event.keyCode <= 90) {
-            checkGuess(event.key.toUpperCase()); 
+            check(event.key.toUpperCase()); 
             updateScreen();
             isWinner();
             isLoser();
@@ -129,11 +151,11 @@ document.onkeyup = function(event) {
     }
 };
 
-
 setup();
 updateScreen();
 
 console.log(ansWord);
+
 
 
 
